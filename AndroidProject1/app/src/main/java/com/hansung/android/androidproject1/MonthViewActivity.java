4e34a74a6e69e29ca83592177c553fbe6e64555f
ParkSeongJun <sjyj98@naver.com>
2022-04-03 15:05:15 +0900

    toastmessage

--- AndroidProject1/app/src/main/java/com/hansung/android/androidproject1/MonthViewActivity.java
+++ AndroidProject1/app/src/main/java/com/hansung/android/androidproject1/MonthViewActivity.java
@@ -35,7 +35,7 @@ public class MonthViewActivity extends AppCompatActivity {
         year = intent.getIntExtra("year", -1);
         month = intent.getIntExtra("month", -1);
 
-        if (year == -1 || month == -1){
+        if (year == -1 || month == -1) {
             year = cal.get(Calendar.YEAR);
             month = cal.get(Calendar.MONTH);
             day = cal.get(Calendar.DATE);
@@ -43,7 +43,7 @@ public class MonthViewActivity extends AppCompatActivity {
         //날짜 받아오기.
 
         cal.set(year, month, 1);
-        day = cal.get(Calendar.DAY_OF_WEEK)-1;
+        day = cal.get(Calendar.DAY_OF_WEEK) - 1;
         //첫 날 요일 구하기. 0부터 시작해야해서 1을 빼줌.
         //  DAY_OF_WEEK는 Calendar 내 상수로 정의되어있음.
 
@@ -51,23 +51,23 @@ public class MonthViewActivity extends AppCompatActivity {
         // 1일의 앞에 빈 칸 만큼 비워둠.
 
         if (month == 1 && year % 4 != 0)
-            for(int i = 1; i <= 28; i++)
+            for (int i = 1; i <= 28; i++)
                 days.add("" + i);
         // 윤달이 아닌 해 2월 -28일
         if (month == 1 && year % 4 == 0)
-            for(int i = 1; i <= 29; i++)
+            for (int i = 1; i <= 29; i++)
                 days.add("" + i);
         // 윤달 일 시 2월 - 29일
         if (month == 3 || month == 5 || month == 8 || month == 10)
-            for(int i = 1; i <= 30; i++)
+            for (int i = 1; i <= 30; i++)
                 days.add("" + i);
         // 4,6,9,11월 - 30일
         if (month == 0 || month == 2 || month == 4 || month == 6 || month == 7 || month == 9 || month == 11)
-            for(int i = 1; i <= 31; i++)
+            for (int i = 1; i <= 31; i++)
                 days.add("" + i);
         // 1,3,5,7,8,10,12월 - 31일
 
-        YearMonthTV.setText(year + "년 " + (month+1) + "월");
+        YearMonthTV.setText(year + "년 " + (month + 1) + "월");
         //연월표시.
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
@@ -75,6 +75,17 @@ public class MonthViewActivity extends AppCompatActivity {
                 days);
         gridView.setAdapter(adapter);
         //달력 날짜부분 표시.
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                if (position >= day) { // 1일보다 앞이면 반응하지 않기.
+                    Toast.makeText(MonthViewActivity.this,
+                            year + "." + (month + 1) + "." + (position - day + 1),
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
     }
 }
